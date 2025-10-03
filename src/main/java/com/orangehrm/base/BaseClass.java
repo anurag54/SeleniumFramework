7e91a30317a27c63a295ce9410554aae7c9c3609
ANURAG VERMA <anurag.verma54@gmail.com>
2025-10-03 23:35:10 +0530

    Initial commit for UI AT

---
+++ src/main/java/com/orangehrm/base/BaseClass.java
@@ -0,0 +1,93 @@
+package com.orangehrm.base;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+public class BaseClass {
+
+	protected static Properties prop;
+	protected static WebDriver driver;
+	
+	// This method can be used to load configuration if needed separately
+	
+	@BeforeSuite
+	public void loadConfig() throws IOException {
+		prop = new Properties();
+		FileInputStream fis = new FileInputStream("src/main/resources/config.properties");
+		prop.load(fis);
+	}
+	
+	@BeforeMethod
+	public void setup() throws IOException {
+		launchBrowser();
+		configureBrowser();
+		staticWait(2); // Static wait to allow the browser to stabilize
+	}
+	
+	// Initialize the webDrive based on the browser defined in properties file
+	private void launchBrowser() {
+		// This method can be used to launch browser if needed separately
+		String browser = prop.getProperty("browser");
+
+		if (browser.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		} else if (browser.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+		} else {
+			throw new IllegalArgumentException("Browser not supported: " + browser);
+		}
+	}
+	
+	// Configure the browser settings
+	private void configureBrowser() {
+		// Implicit wait
+		int implicitWait = Integer.parseInt(prop.getProperty("implicitWait"));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(implicitWait));
+
+		// Launch the URL
+		try {
+			driver.get(prop.getProperty("url"));
+		} catch (Exception e) {
+			System.out.println("Unable to launch the URL: " + e.getMessage());
+		}
+		
+
+		// maximize the browser window
+		driver.manage().window().maximize();
+	}
+	
+	// Quit will close all the browser windows opened by WebDriver,
+	// Close will close only the current window
+	
+	@AfterMethod
+	public void tearDown() {
+		if (driver != null) {
+			try {
+				driver.quit();
+			} catch (Exception e) {
+				System.out.println("Error while quitting the browser: " + e.getMessage()); 
+			} 
+							
+		}
+	}
+	
+	//Static wait for pause execution for a specified time
+	public void staticWait(int seconds) {
+		LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(seconds));
+	}
+
+}
---
+++ src/test/java/com/orangehrm/test/DummyTest.java
@@ -0,0 +1,23 @@
+package com.orangehrm.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.orangehrm.base.BaseClass;
+
+public class DummyTest extends BaseClass {
+
+	@Test
+	public void dummyTest() {
+		// Navigate to the OrangeHRM application URL before checking the title
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"); // TODO: Replace with actual OrangeHRM URL
+
+		// Wait for the title to be 'OrangeHRM' (up to 10 seconds)
+		new org.openqa.selenium.support.ui.WebDriverWait(driver, java.time.Duration.ofSeconds(10))
+			.until(d -> d.getTitle().equals("OrangeHRM"));
+
+		String title = driver.getTitle();
+		Assert.assertEquals(title, "OrangeHRM");
+		System.out.println("Title verified: " + title);
+	}
+}
